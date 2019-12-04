411a165d9c87787e3c53c551a7f7dce08a11711a
18865091603 <1358404475@qq.com>
2019-12-04 08:34:40 +0800

    commit-002

--- app/src/main/java/com/devyk/customview/sample_1/ScrollerSample_1.kt
+++ app/src/main/java/com/devyk/customview/sample_1/ScrollerSample_1.kt
@@ -13,7 +13,7 @@ import android.widget.Scroller
  *     blog    : https://juejin.im/user/578259398ac2470061f3a3fb/posts
  *     github  : https://github.com/yangkun19921001
  *     mailbox : yang1001yk@gmail.com
- *     desc    : This is ScrollerSample_1
+ *     desc    : This is ScrollerSample
  * </pre>
  */
 
